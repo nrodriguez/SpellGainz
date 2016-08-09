a8dc26073d081d326aa04b954829023a75ba6905
Nephtali Rodriguez <nrodriguez@xogrp.com>
2016-08-09 13:40:31 -0400

    Adds ace 3

---
+++ Core.lua
@@ -0,0 +1,13 @@
+WelcomeHome = LibStub("AceAddon-3.0"):NewAddon("SpellGainz", "AceConsole-3.0")
+
+function WelcomeHome:OnInitialize()
+    self:Print("Hello World!")
+end
+
+function WelcomeHome:OnEnable()
+    -- Called when the addon is enabled
+end
+
+function WelcomeHome:OnDisable()
+    -- Called when the addon is disabled
+end
