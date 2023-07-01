b4103759691dbeff5615dc030a024a335e8f8dd0
Nephtali Rodriguez <nerodriguez@shutterstock.com>
2023-07-01 02:45:48 -0500

    * Updates the logic to properly ignore dragonflight spells. * Adds config options to hide gainz window and to clear it out of previous gainz

--- Core.lua
+++ Core.lua
@@ -16,7 +16,7 @@ function SpellGainz:OnEnable()
 end
 
 function SpellGainz:OnDisable()
-  -- Called when the addon is disableds
+  -- Called when the addon is disabled
 end
 
 ---All setup for those gainz
@@ -49,11 +49,26 @@ function SpellGainz:SetupSlashConfig()
         type = "execute",
         func = function() f:Show() end
       },
-      -- config = {
-      --   name = "Configure",
-      --   desc = "Configure your gainz panel",
+      hide = {
+        name = "Hide Gainz",
+        desc = "Hide your gainz",
+        type = "execute",
+        func = function() f:Hide() end
+      },
+      clear = {
+        name = "Clear Gainz",
+        desc = "Clear out previous gainz",
+        type = "execute",
+        func = function() f:ReleaseChildren() end
+      },
+      --Used for debugging specific spells
+      -- test = {
+      --   name = "Test Gainz",
+      --   desc = "Test your gainz",
       --   type = "execute",
-      --   func = function() InterfaceOptionsFrame_OpenToCategory("SpellGainz") end
+      --   func = function(info, input)
+      --     SpellGainz:MIMIC_SPELL_LEARN("eventName", 372608)
+      --   end,
       -- }
     },
   }
@@ -73,9 +88,7 @@ function SpellGainz:CHAT_MSG_SYSTEM(eventName, message)
   end
 end
 
-function SpellGainz:LEARNED_SPELL_IN_TAB(eventName, spellID)
-  local name, rank, icon, castingTime, minRange, maxRange, spellID, originalIcon = GetSpellInfo(spellID)
-
+function SpellGainz:IS_DRAGONRIDING_SPELL(spellID)
   --Ignore Dragonriding Spells
   local dragonRidingSpellIds = {
     372608, --Surge Forward
@@ -83,9 +96,31 @@ function SpellGainz:LEARNED_SPELL_IN_TAB(eventName, spellID)
     374990, --Bronze Timelock
     361584, --Whirling Surge
     403092  -- Aerial Halt
+
   }
 
-  if not IsPassiveSpell(spellID) and not dragonRidingSpellIds[spellID] then
+  for _, v in pairs(dragonRidingSpellIds) do
+    if v == spellID then
+      return true
+    end
+  end
+  return false
+end
+
+--Used for testing specific spells
+-- function SpellGainz:MIMIC_SPELL_LEARN(eventName, spellID)
+--   local name, rank, icon, castingTime, minRange, maxRange, spellID, originalIcon = GetSpellInfo(spellID)
+
+--   if not IsPassiveSpell(spellID) and not SpellGainz:IS_DRAGONRIDING_SPELL(spellID) then
+--     SpellGainz:AddNewSpell(spellID)
+--     f:Show()
+--   end
+-- end
+
+function SpellGainz:LEARNED_SPELL_IN_TAB(eventName, spellID)
+  local name, rank, icon, castingTime, minRange, maxRange, spellID, originalIcon = GetSpellInfo(spellID)
+
+  if not IsPassiveSpell(spellID) and not SpellGainz:IS_DRAGONRIDING_SPELL(spellID) then
     SpellGainz:AddNewSpell(spellID)
     f:Show()
   end
